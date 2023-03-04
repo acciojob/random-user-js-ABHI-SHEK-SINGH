12615a524e9037e304c4ec47b0dbcbbc7905ba93
ABHI-SHEK-SINGH <104828515+ABHI-SHEK-SINGH@users.noreply.github.com>
2023-03-04 23:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,6 +26,9 @@ function showPhone(){
 function showGetUser(){
 	document.getElementById('showDetails').innerText = "";
 	 getRadomUser()
+	for(let i=1;i<user.length;i++){
+		document.getElementById('showChange_img').src = `${user.picture.medium}`
+	}
 	// document.getElementById('showChange_img').src = `${user.picture.medium`
 	
 }
