6f34cafcfe6a60178dcd807cb5f650733a1ca11f
ABHI-SHEK-SINGH <104828515+ABHI-SHEK-SINGH@users.noreply.github.com>
2023-03-04 23:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 //your code here
-  function getRadomUser() {
+var user;
+ async function getRadomUser() {
 	const response = await fetch('https://randomuser.me/api/');
 	 const data = await response.json();
-	 const user = data.results[0];
+	  user = data.results[0];
 	 displayUser(user);
 }
 
 function displayUser(user){
 document.getElementById('fullname').innerText = `${user.name.first} ${user.name.last}`
-
-document.getElementById('age').innerText = `${user.registered.age}`
-	document.getElementById('email').innerText = 
-	document.getElementById('phone')
 }
+	
+function showAge(){
+	document.getElementById('showDetails').innerText = `${user.registered.age}`
+}
+	
+function showEmail(){
+	document.getElementById('showDetails').innerText = `${user.email}`
+}
+
+
 getRadomUser();
